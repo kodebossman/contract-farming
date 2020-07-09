e0cd838bb8905cea315cb0e6180cfb8d1c7116db
kudakwashe <kuda@nightsbridge.co.za>
2020-07-09 20:55:34 +0200

    Master- Initial commit to the Central Repository

---
+++ src/main/java/com/cb/contractfarming/ContractFarmingApplication.java
@@ -0,0 +1,13 @@
+package com.cb.contractfarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractFarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractFarmingApplication.class, args);
+	}
+
+}
