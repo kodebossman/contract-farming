846bb7447c6058a9011b89f11e925c50a5677ed4
Chance der Fritz <chancethefritz@gmail.com>
2020-07-15 11:00:15 +0200

    Added the service package for Contracts, changed index of Contractor from id to msisdn (contractorPhoneNumber in this case). Also I'm getting an error: Cannot add foreign key contractor_id to reference contractor in contracts table.

--- src/main/java/com/cb/contractfarming/contractor/Contractor.java
+++ src/main/java/com/cb/contractfarming/contractor/Contractor.java
@@ -10,7 +10,7 @@ import lombok.ToString;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "contractor", indexes = {@Index(name = "index_id", columnList = "id", unique = true)})
+@Table(name = "contractor", indexes = {@Index(name = "indx_contractor", columnList = "contractorPhoneNumber", unique = true)})
 @Getter
 @Setter
 @ToString
@@ -24,12 +24,11 @@ public class Contractor extends BaseEntity {
     @Column(name = "contractorType", nullable = false, length = 20)
     private ContractorType type;
 
-    @Column(name = "contractorPhoneNumber", nullable = false, length = 15)
+    @Column(name = "contractorPhoneNumber", nullable = false, length = 15, unique = true)
     private String phoneNumber;
 
     @Column(name = "contractorPhysicalAddress", nullable = false, length = 150)
     private String physicalAddress;
-
 
     @Column(name = "contractorEmail", nullable = false, length = 45)
     private String email;
