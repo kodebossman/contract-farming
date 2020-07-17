78560123362275ac5badb5b8eee21daaac0a5a12
Garwe <67763320+Garwe@users.noreply.github.com>
2020-07-17 16:13:09 +0200

    Created an Items API

--- src/main/java/com/cb/contractfarming/config/TypeMapper.java
+++ src/main/java/com/cb/contractfarming/config/TypeMapper.java
@@ -3,17 +3,20 @@ package com.cb.contractfarming.config;
 
 import com.cb.contractfarming.farmer.model.Farmer;
 import com.cb.contractfarming.farmer.model.FarmerDto;
+import com.cb.contractfarming.items.Item;
+import com.cb.contractfarming.items.ItemsDTO;
 import org.mapstruct.Mapper;
 
 
 @Mapper(componentModel = "spring")
-public interface TypeMapper {
+public interface  TypeMapper {
 
   Farmer map(FarmerDto farmerDto);
 
   FarmerDto map(Farmer farmer);
 
 
-
+  Item map(ItemsDTO itemsDTO);
+  ItemsDTO map(Item item);
 }
 
