7d1f42e62242d6fe7c4c4e1a5957bea2f8f457eb
Linval <tlinvalchisoko@gmail.com>
2020-07-20 15:12:40 +0200

    contract cost api

--- src/main/java/com/cb/contractfarming/contractcost/model/api/contractcostApi.java
+++ src/main/java/com/cb/contractfarming/contractcost/model/api/contractcostApi.java
@@ -7,20 +7,21 @@ import com.cb.contractfarming.config.TypeMapper;
 import com.cb.contractfarming.contractcost.model.ContractualCost;
 import com.cb.contractfarming.contractcost.model.CostDto;
 import com.cb.contractfarming.contractcost.model.service.ContractCostService;
+import com.cb.contractfarming.project.ProjectDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/contractcost.model")
+@RequestMapping("/contract")
 
 public class contractcostApi implements CrudApi<CostDto> {
     private ContractCostService contractCostService;
@@ -39,13 +40,24 @@ public class contractcostApi implements CrudApi<CostDto> {
     }
 
 
-    @Override
+    @PostMapping(value = "/create")
     public ApiResponse<CostDto> create(CostDto costDto) {
         //log.info("contractual cost creation: {}", costDto);
         final ContractualCost contractualCost = ContractCostService.register(typeMapper.map(costDto));
         return new ApiResponse<>(HttpStatus.OK.value(), typeMapper.map(contractualCost));
 
     }
+   /** public ApiResponse<PaginationResult<CostDto> > findById(@PathVariable(name = "name") Long Id){
+
+        List<CostDto> costDtos =  contractCostService.findById(Id)
+                .stream()
+                .map(typeMapper::map)
+                .collect(Collectors.toList());
+
+        PaginationResult<CostDto> pagedcosts = PaginationResult.pagination(costDtos, contractCostService.total(), 1 , costDtos.size()-1);
+
+        return new ApiResponse<>(HttpStatus.OK.value(), pagedcosts);
+    }**/
 
     @Override
     public ApiResponse<CostDto> update(CostDto costDto) {
